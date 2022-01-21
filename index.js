40e8f64ced2323d9fed8e44a2fd7b49811ca5c14
Melisa500 <80432492+Melisa500@users.noreply.github.com>
2022-01-21 12:04:26 -0300

    add express (#1)

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const http = require('http')
+const express = require('express')
+const server = express()
 
-const server = http.createServer((req,res) =>{
-    
+
+
+server.get('/', (req, res) =>{
+    res.send('<h1>Hola</h1>')
+})
+
+server.get('/api/notes', (req, res) =>{
+    res.json(notes)
+})
+
+
+
+const PORT = 8080
+server.listen(PORT, () =>{
+    console.log(`ESCUCHO PUERTO ${PORT}`)
 })
